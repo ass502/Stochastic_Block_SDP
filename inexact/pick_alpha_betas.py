80370c6e3d83d25481df283907b32a8f1cfa2eb4
Aditi Nair <asn264@nyu.edu>
2017-05-08 21:59:34 -0400

    a b script

--- inexact/pick_alpha_betas.py
+++ inexact/pick_alpha_betas.py
@@ -4,9 +4,9 @@ import math
 def test_bound(a,b,k):
 
 	if k==2:
-		return (a-b)**2 > 2(a+b) #KASTEN-STEIGEN BOUND
+		return (a-b)**2 > 2*(a+b) #KASTEN-STEIGEN BOUND
 	else:
-		return (a-b)**2/float(k(a+(k-1)b)) > 1
+		return (a-b)**2/float(k*(a+(k-1)*b)) > 1
 
 k = 2
 m = 100
@@ -15,14 +15,13 @@ n = float(k*m)
 b_values = np.arange(0,.8,.08)
 a_values = np.arange(1.6,6.6,.5)
 
-
 counter = 0.
 for b in b_values:
 	for a in a_values:
 		counter += test_bound(a,b,k)
 		print a/n, b/n
 		
-tot=len(alpha_values)*len(beta_values)
+tot=len(a_values)*len(b_values)
 
 print counter/tot
-print len(alpha_values), len(beta_values), tot
+print len(a_values), len(b_values), tot
